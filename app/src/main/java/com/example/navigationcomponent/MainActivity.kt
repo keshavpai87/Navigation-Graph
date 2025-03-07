c9461c23f550a6d796cdea670de662ae51cae2ca
Keshav Pai <keshu.pai@gmail.com>
2025-03-07 08:25:29 +0530

    Navigation Graph Added

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -2,46 +2,15 @@ package com.example.navigationcomponent
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.navigationcomponent.ui.theme.NavigationComponentTheme
+import androidx.databinding.DataBindingUtil
+import com.example.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            NavigationComponentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationComponentTheme {
-        Greeting("Android")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
